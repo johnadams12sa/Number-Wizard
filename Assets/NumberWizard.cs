e5d28c8f6ddf5562d63bbf711ab372ea61027de4
Aaron Yam <ayam5005@gmail.com>
2019-02-02 11:42:57 -0500

    Revisions to NumberWizard Script

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -11,12 +11,13 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Welcome to Number Wizard, yo");
-        Debug.Log("Pick a number: ");
+        Debug.Log("Yo whaddup man, welcome to Number Wizard");
+        Debug.Log("Pick a number, any number: ");
         Debug.Log("Lowest number is: " + min);
         Debug.Log("Highest Number is: " + max);
-        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Tell me if your number is higher or lower than " + guess);
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        max = max + 1;
     }
 
     // Update is called once per frame
@@ -24,18 +25,21 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Up Arrow key was pressed.");
             min = guess;
-            Debug.Log(guess);
+            guess = (max + min) / 2;
+            Debug.Log("Is your number is higher than " + guess);
         }
 
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Down Arrow key was pressed.");
+            max = guess;
+            guess = (max + min) / 2;
+            Debug.Log("Is your number is lower than " + guess);
         }
+
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("You hit Enter");
+            Debug.Log("I am a genius! ");
         }
     }
 }
